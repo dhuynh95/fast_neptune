225a26a024729e85c5be9d93b29f7366c16045ce
DanielWinny <daniel.quoc.huynh@gmail.com>
2020-01-24 08:35:16 +0100

    First commit

---
+++ 00_core.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#hide
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+
+# In[2]:
+
+
+# default_exp core
+
+
+# In[3]:
+
+
+#export
+from nbdev.export import check_re,read_nb
+from pathlib import Path
+import re
+import os
+import platform
+
+
+# # module name here
+# 
+# > API details.
+
+# ## Code cells
+
+# In[4]:
+
+
+#export
+_re_blank_code = re.compile(r"""
+# Matches any line with #export or #exports without any module name:
+^         # beginning of line (since re.MULTILINE is passed)
+\s*       # any number of whitespace
+\#\s*     # # then any number of whitespace
+code  # export or exports
+\s*       # any number of whitespace
+$         # end of line (since re.MULTILINE is passed)
+""", re.IGNORECASE | re.MULTILINE | re.VERBOSE)
+
+_re_mod_code = re.compile(r"""
+# Matches any line with #export or #exports with a module name and catches it in group 1:
+^         # beginning of line (since re.MULTILINE is passed)
+\s*       # any number of whitespace
+\#\s*     # # then any number of whitespace
+code  # export or exports
+\s*       # any number of whitespace
+(\S+)     # catch a group with any non-whitespace chars
+\s*       # any number of whitespace
+$         # end of line (since re.MULTILINE is passed)
+""", re.IGNORECASE | re.MULTILINE | re.VERBOSE)
+
+
+# In[5]:
+
+
+#export
+def is_code(cell, default="main.py"):
+    "Check if `cell` is to be exported and returns the name of the module to export it if provided"
+    if check_re(cell, _re_blank_code):
+        return default
+    tst = check_re(cell, _re_mod_code)
+    return os.path.sep.join(tst.groups()[0].split('.')).replace("\\",".") if tst else None
+
+
+# In[6]:
+
+
+#export
+from collections import defaultdict
+
+def get_codes(fn:str,default:str = "main.py") -> dict:
+    nb = read_nb(fn)
+    
+    module_to_code = defaultdict(str)
+    
+    module_to_code[default] = ""
+    
+    for cell in nb["cells"]:
+        code = is_code(cell,default)
+        if code:
+            module_to_code[code] += cell["source"]
+    
+    return dict(module_to_code)
+
+
+# ## Properties
+
+# ### OS information
+
+# In[7]:
+
+
+#export
+def get_metadata() -> dict:
+    data = {
+        "os":os.name,
+        "system":platform.system(),
+        "release":platform.release(),
+        "python_version":platform.python_version()
+    }
+    return data
+
+
+# In[8]:
+
+
+get_metadata()
+
+
+# ### Modules information
+
+# In[9]:
+
+
+#export
+def create_requirements(fn):
+    # Convert the notebook to a python file
+    os.system(f"jupyter nbconvert --to=python {fn}")
+    
+    # Create the requirements file
+    os.system("pipreqs ./ --force")
+
+
+# ### Properties cells
+
+# In[10]:
+
+
+#export
+_re_blank_property = re.compile(r"""
+# Matches any line with #export or #exports without any module name:
+^         # beginning of line (since re.MULTILINE is passed)
+\s*       # any number of whitespace
+\#\s*     # # then any number of whitespace
+property  # export or exports
+\s*       # any number of whitespace
+$         # end of line (since re.MULTILINE is passed)
+""", re.IGNORECASE | re.MULTILINE | re.VERBOSE)
+
+_re_obj_def = re.compile(r"""
+# Catches any 0-indented object definition (bla = thing) with its name in group 1
+^          # Beginning of a line (since re.MULTILINE is passed)
+([^=\s]*)  # Catching group with any character except a whitespace or an equal sign
+\s*=       # Any number of whitespace followed by an =
+""", re.MULTILINE | re.VERBOSE)
+
+
+# In[11]:
+
+
+#export
+def is_property(cell):
+    "Check if `cell` is to be exported and returns the name of the module to export it if provided"
+    if check_re(cell, _re_blank_property):
+        return True
+    else:
+        return False
+
+def add_cell_to_properties(cell: dict,properties: dict):
+    """Adds all variables in the cell to the properties"""
+    objs = _re_obj_def.findall(cell["source"])
+    
+    objs = {obj : globals()[obj] for obj in objs}
+    
+    properties.update(objs)
+
+
+# In[12]:
+
+
+#export
+def files_in_properties(properties:dict):
+    """Returns the list of files from properties"""
+    files = []
+    for key,val in properties.items():
+        if isinstance(val,Path) and val.is_file():
+            files.append(str(val))
+    return files
+
+
+# In[13]:
+
+
+#export
+def get_properties_from_cells(fn: str,return_files:bool = True):
+    """Gets the properties from all #property cells"""
+    
+    nb = read_nb(fn)
+    
+    properties = {}
+    
+    for cell in nb["cells"]:
+        if is_property(cell):
+            add_cell_to_properties(cell,properties)
+
+    files = files_in_properties(properties)
+    return properties,files
+
+
+# ## Wrapper
+
+# In[36]:
+
+
+#export
+from contextlib import contextmanager
+from neptune.projects import Project
+
+@contextmanager
+def fast_experiment(project: Project,nb_name:str,return_files: bool = True,
+                    default:str = "main.py",**kwargs):
+    # First we get the code cells
+    codes = get_codes(nb_name,default=default)
+    
+    # We write them in separate files
+    for fn,code in codes.items():
+        with open(fn,"w") as file:
+            file.write(code)
+            
+    codes = list(codes.keys())
+    
+    # We get the properties
+    properties,files = get_properties_from_cells(nb_name,return_files=return_files)
+    metadata = get_metadata()
+    properties.update(metadata)
+    properties["nb_name"] = nb_name
+    
+    # We convert the dict keys to string
+    for k,v in properties.items():
+        properties[k] = str(v)
+    
+    exp = project.create_experiment(params=properties,upload_source_files=codes,**kwargs)
+    
+    # We create the requirements file and send it
+    create_requirements(nb_name)
+    exp.send_artifact("requirements.txt")
+    
+    for fn in files:
+        exp.send_artifact(fn)
+        
+    yield exp
+    
+    exp.stop()
+    
+    # We remove the code files
+    for fn in codes:
+        os.remove(fn)
+
+
+# In[15]:
+
+
+#property
+fn = "00_core.ipynb"
+
+
+# In[16]:
+
+
+#code
+print("coucou")
+
+
+# In[ ]:
+
+
+#code load.py
+print("loaded")
+
+
+# In[ ]:
+
+
+#property
+coucou = "1337"
+
+
+# In[17]:
+
+
+get_properties_from_cells(fn)
+
+
+# In[18]:
+
+
+get_codes(fn)
+
+
+# In[34]:
+
+
+# Neptune login
+from neptune.sessions import Session
+import getpass
+
+api_token = getpass.getpass("Please enter your NeptuneML API token : ")
+session = Session(api_token=api_token)
+project = session.get_project(project_qualified_name='danywin/EnsembleDropout')
+
+
+# In[35]:
+
+
+name = "test"
+with fast_experiment(project,nb_name=fn,name=name) as exp:
+    pass
+
+
+# In[30]:
+
+
+get_ipython().system('pip install psutil ')
+
--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-# nbdev template
+# Project name here
+> Summary description here.
 
-Use this template to more easily create your nbdev project.
 
+This file will become your README and also the index of your documentation.
+
+## Install
+
+`pip install your_project_name`
+
+## How to use
+
+Fill me in please! Don't forget code examples:
+
+```python
+# Neptune login
+from neptune.sessions import Session
+import getpass
+
+api_token = getpass.getpass("Please enter your NeptuneML API token : ")
+session = Session(api_token=api_token)
+project = session.get_project(project_qualified_name='danywin/fast-neptune')
+```
+
+    Please enter your NeptuneML API token : ········
+    
+
+    WARNING: Instantiating Session without specifying a backend is deprecated and will be removed in future versions. For current behaviour use `neptune.init(...)` or `Session.with_default_backend(...)
+    WARNING: It is not secure to place API token in your source code. You should treat it as a password to your account. It is strongly recommended to use NEPTUNE_API_TOKEN environment variable instead. Remember not to upload source file with API token to any public repository.
+    
+
+```python
+name = "MNIST-example"
+description = "Demonstration of fast_neptune on MNIST using fastai"
+```
