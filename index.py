e648bd1a6f0ce02cff0f329a274afe378510bb1c
Daniel Huynh <dhuynh@endymion.enst.fr>
2020-01-28 14:16:59 +0100

    Update index

--- index.py
+++ index.py
@@ -15,27 +15,20 @@
 
 # Fill me in please! Don't forget code examples:
 
-# In[6]:
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+
+# In[2]:
 
 
 from fast_neptune.core import *
 
 
-# In[7]:
-
-
-#code
-print("Coucou")
-
-
-# In[8]:
-
-
-nb_name = "index.ipynb"
-get_codes(nb_name)
-
-
-# In[9]:
+# In[3]:
 
 
 # Neptune login
@@ -44,20 +37,58 @@ import getpass
 
 api_token = getpass.getpass("Please enter your NeptuneML API token : ")
 session = Session(api_token=api_token)
-project = session.get_project(project_qualified_name='danywin/EnsembleDropout')
+user_name = "danywin"
+project_name = "fast-neptune"
+project = session.get_project(project_qualified_name=f'{user_name}/{project_name}')
 
 
-# In[13]:
+# In[4]:
+
+
+#code
+from fastai.vision import *
+
+path = untar_data(URLs.MNIST_SAMPLE)
+data = ImageDataBunch.from_folder(path)
+learn = cnn_learner(data, models.resnet18, metrics=accuracy)
+
+
+# In[5]:
+
+
+#property
+n_epoch = 1
+lr = 1e-2
+
+
+# In[6]:
 
 
 nb_name = "index.ipynb"
 globs = globals()
+
 with fast_experiment(project,nb_name,globs) as exp:
-    pass
+    learn.fit_one_cycle(n_epoch,lr)
+    pred,y = learn.get_preds()
+    
+    score = accuracy(pred,y).item()
+    exp.send_metric("valid_accuracy",score)
 
 
-# In[ ]:
+# In[15]:
 
 
+get_properties_from_cells(nb_name,globs)
 
+
+# In[16]:
+
+
+get_codes(nb_name)
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('pinfo2', 'fast_experiment')
 
